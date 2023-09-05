a328fca407f0ebfa96d99e1caba267939140a611
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-09-05 08:18:14 -0300

    feat: base made

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+class Container{
+    constructor(name, fixo, celular, image, calendario, email, cep, cidade, insta, git){
+        this.name=name;
+        this.fixo=fixo;
+        this.celular= celular;
+        this.image= image;
+        this.calendario= calendario;
+        this.email= email;
+        this.cep=cep;
+        this.cidade=cidade;
+        this.insta=insta;
+    }
+}
